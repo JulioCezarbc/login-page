25adc0d02e45062f9c3174e70c3f6424a6de23d0
JulioCezarbc <julio04.cordeiro@gmail.com>
2024-07-23 01:04:04 -0300

    Criando um auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { UsersComponent } from './pages/users/users.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
     {
@@ -10,5 +12,10 @@ export const routes: Routes = [
     {
         path: "signup",
         component: SignUpComponent
+    },
+    {
+        path: "user",
+        component: UsersComponent,
+        canActivate: [AuthGuard]
     }
 ];
